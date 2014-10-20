58e4b4dfbed74b835472a823d5a940dcf53371c4
leo <420760135@qq.com>
2014-10-20 23:09:43 +0800

    receive post stream form client

--- client_listener.php
+++ client_listener.php
@@ -1,3 +1,13 @@
 <?php
-
+//echo "test code: ".$_POST["test"]."\n";
+//echo "test file: ".$_FILES["file"];
+//var_dump($_REQUEST);
+$string_img = $_POST["file"];
+$file_name = $_POST["file_name"];
+$img = imagecreatefromstring($string_img);
+imagejpeg($img,$file_name);
+// echo <<<EOT
+	// <img src="1.jpg">
+// EOT;
+echo $file_name." is uploaded.";
 ?>
