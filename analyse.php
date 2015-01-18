d9da4c55d8739cf7f0975ff9bb9669438e6b682d
leo <420760135@qq.com>
2015-01-18 18:57:44 +0800

    add choose_max & selector

--- analyse.php
+++ analyse.php
@@ -14,11 +14,14 @@ if($type=="search")
 	$grayimg = "gray/gray-".basename($file_dir);
 	grayjpg($file_dir, $grayimg);
 	$detect_result = array();
-
-	for($i=0;$i<count($xml);$i++)
+	$detect_result[0] = choose_max(face_detect($grayimg, $xml[0]));
+	for($i=1;$i<count($xml);$i++)
 	{
-		$detect_result[$i] = face_detect($grayimg, $xml[$i]);
+		
+		$detect_result[$i] = choose_max(selector($detect_result[0],face_detect($grayimg, $xml[$i])));
 	}
+	
+	include "test.php";
 	/*
 	echo <<<EOT
 		<img src="{$grayimg}">
@@ -34,5 +37,39 @@ else
 	include_once "insert.php";
 }
 
+function choose_max($features)
+{
+	$result = array();
+	$max = 0;
+	for ($i = 0; $i < count($features); $i++)
+	{
+		if ($features[$i]["w"]*$features[$i]["h"]>$max)
+		{
+			$max = $features[$i]["w"]*$features[$i]["h"];
+			$result = $features[$i];
+		}
+		
+	}
+	return $result;
+	
+}
+
+function selector($face,$features)
+{
+	$temp = array();
+	for ($i = 0,$j = 0; $i < count($features); $i++)
+	{
+		if ($features[$i]["x"]>=$face["x"]&&$features[$i]["y"]>=$face["y"]&&
+		($features[$i]["x"]+$features[$i]["w"])<=($face["x"]+$face["w"])&&
+		($features[$i]["y"]+$features[$i]["h"])<=($face["y"]+$face["h"]))
+		{
+			$temp[$j] = $features[$i];
+			$j++;
+			
+		}
+			
+	}
+	return $temp;
+}
 
 ?>
